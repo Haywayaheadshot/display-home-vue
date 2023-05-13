93684e4346e73053d53c4cd9b78da332a056dac2
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-13 01:01:28 +0000

    Try to maintain height with flex-shrink and media queries

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,10 +10,12 @@
       button 3
     </button>
   </div>
-  <div class="div-2">
-    <b class="images">1,20000</b>
+  <div>
+    <div class="div-2">
+      <b class="images">1,20000</b>
     <b class="images">Jobs</b>
     <b class="images">For you</b>
+    </div>
   </div>
 </template>
 
@@ -36,10 +38,33 @@
   background-image: url(../assets/vue.svg);
   background-position: center;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: contain;
+  margin-top: 30px;
 }
 
 .images {
   padding: 20vh 10vw;
+  flex-shrink: 6;
 }
+
+/* Media query for screens less than 600px wide */
+/* @media screen and (max-width: 600px) {
+  .div-2 {
+    margin-top: 20px;
+  }
+} */
+
+/* Media query for screens between 600px and 900px wide */
+/* @media screen and (min-width: 600px) and (max-width: 900px) {
+  .div-2 {
+    margin-top: 40px;
+  }
+} */
+
+/* Media query for screens greater than 900px wide */
+/* @media screen and (min-width: 900px) {
+  .div-2 {
+    margin-top: 50px;
+  }
+} */
 </style>
